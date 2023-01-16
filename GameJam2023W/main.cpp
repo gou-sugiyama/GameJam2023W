6c6ee9ad2d7a3b98b96cc2499637e0a801c327bf
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2023-01-16 14:09:41 +0900

    Initial commit

---
+++ GameJam2023W/main.cpp
@@ -0,0 +1,40 @@
+#include "DxLib.h"
+#include "common.h"
+
+/***********************************************
+ * プログラムの開始
+ ***********************************************/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// タイトルを test に変更
+	SetMainWindowText("GameJam2023Winter");
+
+	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	SetGraphMode(D_SCREEN_WIDTH, D_SCREEN_HEIGHT, 32);
+
+	if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
+
+	SetMouseDispFlag(FALSE);	//マウスカーソル非表示
+
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
+
+	SetFontSize(20);		// 文字サイズを設定
+
+	// ゲームループ
+	while (ProcessMessage() == 0 &&
+		GetJoypadInputState(DX_INPUT_KEY_PAD1) != PAD_INPUT_9/*ESCキー*/ 
+		)
+	{
+		ClearDrawScreen();
+
+		DrawString(0, 0, "FirstCommit", 0xFFFFFF);
+
+		ScreenFlip();			// 裏画面の内容を表画面に反映
+	}
+
+	DxLib_End();	// DXライブラリ使用の終了処理
+
+	return 0;	// ソフトの終了
+}
