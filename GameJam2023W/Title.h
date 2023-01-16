3a980b5fe457bf906ed523a286b0439827b463f9
杉山豪 <gou-sugiyama-k21f@stu.kbc.ac.jp>
2023-01-17 05:46:01 +0900

    シーンマネージャーの作成
    タイトルシーン、ゲームメインシーンを作りました。
    現在動作確認のため3秒ごとに切り替わるようにしているので、消してから使ってください。

---
+++ GameJam2023W/Title.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "AbstractScene.h"
+
+class Title : public AbstractScene {
+private:
+	int t = 0; //TODO:消してね
+	bool sceneFlg = false; //TODO:消してね
+
+public:
+	Title();
+
+	// デストラクタ
+	~Title() {}
+
+	void Update() override;						//描画以外の更新処理
+	void Draw() const override;					//描画の更新処理
+	AbstractScene* ChangeScene() override;		//シーンの変更処理
+};
