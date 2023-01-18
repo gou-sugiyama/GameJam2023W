c74d7c2c6917de7181475b00d4924cc6d42d38c9
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2023-01-18 14:21:16 +0900

    わすれもの

--- GameJam2023W/Fire.cpp
+++ GameJam2023W/Fire.cpp
@@ -1,1 +1,37 @@
+#include "DxLib.h"
 #include "Fire.h"
+
+//--------------------
+// コンストラクタ
+//--------------------
+Fire::Fire()
+{
+	animTimer = 0;
+	sparks1.anim = new T_Anim[2];
+	sparks1.anim[0].image = LoadGraph("images/sparks/spark_1");
+	sparks1.anim[0].next = 1;
+	sparks1.anim[1].image = LoadGraph("images/sparks/spark_2");
+	sparks1.anim[1].next = 0;
+}
+
+//--------------------
+// デストラクタ
+//--------------------
+
+
+//--------------------
+// 更新
+//--------------------
+void Fire::Update()
+{
+	animTimer++;
+}
+
+//--------------------
+// 描画
+//--------------------
+void Fire::Draw()const
+{
+	DrawRotaGraph(640,360,1.0/180*60,0,
+		(*images).image,)
+}
--- GameJam2023W/Fire.h
+++ GameJam2023W/Fire.h
@@ -1,5 +1,48 @@
 #pragma once
+
+//アニメーション用
+struct T_Anim
+{
+	int image;
+	int next;
+};
+
+struct T_Image
+{
+	T_Anim* anim = nullptr;
+	int current = 0;
+};
+
 class Fire
 {
+private:
+	int x;
+	int y;
+
+	//アニメーション用変数
+	int animTimer;
+	T_Image sparks1 = T_Image();
+	T_Image sparks2 = T_Image();
+	T_Image sparks3 = T_Image();
+	T_Image sparks4 = T_Image();
+	T_Image sparks5 = T_Image();
+
+	T_Image* images[5] =
+	{
+		sparks1,sparks2,sparks3,sparks4,sparks5
+	};
+
+
+public:
+	//コンストラクタ
+	Fire();
+	//デストラクタ
+	~Fire() {};
+
+	//更新
+	void Update();
+	//描画
+	void Draw()const;
+	
 };
 
